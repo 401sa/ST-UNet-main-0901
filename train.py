e4b3bf50f3562c47442029e8710ce83f8fe99755
linkhc <linkhc@foxmail.com>
2022-09-01 18:21:51 +0900

    1234

--- train.py
+++ train.py
@@ -36,6 +36,7 @@ parser.add_argument('--base_lr', type=float, default=0.01,
                     help='segmentation network learning rate')
 parser.add_argument('--img_size', type=int,
                     default=256, help='input patch size of network input')
+
 parser.add_argument('--seed', type=int,
                     default=1234, help='random seed')
 parser.add_argument('--n_skip', type=int,
